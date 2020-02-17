bf4d1652a195efd4158322c3ee5f449242952b1d
singularian <gigagoth@gmail.com>
2020-02-16 21:05:57 -0500

    Updating code

--- primestring.go
+++ primestring.go
@@ -38,6 +38,33 @@ func SieveOfEratosthenes(n int) []int {
       return primes
 }
 
+func IsPrime(n int) bool {
+      if n < 2 {
+              return false
+      }
+
+      for i := 2; i*i <= n; i++ {
+              if n%i == 0 {
+                      return false
+              }
+      }
+      return true
+}
+
+// variations of a prime bit string
+// 100100010000010000000100000000000100000000000001
+// 
+// this is a run of alternating 0's and 1's 
+// the first 00 group is one prime number n bits 0
+// the following 1 bit group is one prime number delineated by n bits 1
+// 00111000001111111000000000001111111111111
+//
+// break the bigint number into constituent groups of 60 numbers
+// then concatenate each block together
+// formula with concatenation groups
+// 421612065005906503886017970712759923748952792881600196162550115244998622073006303405395430776097230328628476580766070593761091990440640513
+// 42161206500590650388601797071275992374895279288160019616255 01152449986220730063034053954307760972303286284765807660705 93761091990440640513
+// 
 func primesToBin(arr []int) int {
    // var i int
    //var j int
@@ -78,7 +105,55 @@ func primesToBin(arr []int) int {
 
 }
 
+func primesToBin2(primenumber int) int {
+
+   var count int  = 0
+   var number int = 2
+   var buffer bytes.Buffer
+   for {
+      if IsPrime(number) {
+         count++
+         buffer.WriteString("1")
+         for j := 0; j < number; j++ {
+            buffer.WriteString("0")
+         }
+
+      }
+      if count == primenumber {
+         break
+      }
+      number++
+   }
+
+   buffer.WriteString("1")
+
+   fmt.Println("prime bitstring buffer ", buffer.String())
+
+   n := new(big.Int)
+   n, ok := n.SetString(buffer.String(), 2)
+   if !ok {
+        fmt.Println("SetString: error")
+        return 0 
+   }
+
+
+   fmt.Println("prime bitstring bigint", n)
+
+   return 0
+}
+
 func main() {
+
+      var argsNum int = len(os.Args)
+
+      if argsNum < 2 { 
+		fmt.Println("Primestring Usage: \n")
+                fmt.Println(os.Args[0],  "[Number]")
+		fmt.Println("\nserver$ ./primestring 11")
+                fmt.Println("prime bitstring buffer 1001000100000100000001000000000001000000000000010000000000000000010000000000000000000100000000000000000000000100000000000000000000000000000100000000000000000000000000000001")
+                fmt.Println("prime bitstring bigint 3391050601613190613378084052422250933492987908325377")
+		os.Exit(1)
+      }
 
       // argCount := len(os.Args[1:])
       for i := range os.Args {
@@ -87,7 +162,8 @@ func main() {
          // fmt.Println("args ", i, arg)
          p, _ := strconv.Atoi(arg)
          if p != 0 {
-             primesToBin(SieveOfEratosthenes(p))
+         //    primesToBin(SieveOfEratosthenes(p))
+             primesToBin2(p)
          }
       }
 }
